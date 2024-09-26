bce9845c2a5c17dfdc1a02b1114473c9d458ab55
dosukoi <project.lrium@gmail.com>
2024-09-26 10:40:52 +0800

    注册接口参数校验

--- src/main/java/com/lrium/controller/UserController.java
+++ src/main/java/com/lrium/controller/UserController.java
@@ -3,30 +3,33 @@ package com.lrium.controller;
 import com.lrium.pojo.Result;
 import com.lrium.pojo.User;
 import com.lrium.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username,String password){
-        //查询用户
-        User u = userService.findByUserName(username);
-        if (u==null){
-            //没有占用
-            userService.register(username,password);
-            return Result.success();
-        }else{
-            //占用
-            return Result.error("用户名已被占用");
-        }
-        //注册
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username,@Pattern(regexp = "^\\S{5,16}$")  String password){
+            //查询用户
+            User u = userService.findByUserName(username);
+            if (u == null) {
+                //没有占用
+                //注册
+                userService.register(username, password);
+                return Result.success();
+            } else {
+                //占用
+                return Result.error("用户名已被占用");
+            }
     }
 }
