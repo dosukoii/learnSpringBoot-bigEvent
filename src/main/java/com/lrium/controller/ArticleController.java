8331bd48cfa766076a66aa9aad18be8688da66ad
dosukoi <project.lrium@gmail.com>
2024-09-26 12:21:18 +0800

    登录& jwt测试

---
+++ src/main/java/com/lrium/controller/ArticleController.java
@@ -0,0 +1,16 @@
+package com.lrium.controller;
+
+import com.lrium.pojo.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+
+    @GetMapping("/list")
+    public Result<String> list(){
+        return Result.success("all data of article");
+    }
+}
--- src/main/java/com/lrium/controller/UserController.java
+++ src/main/java/com/lrium/controller/UserController.java
@@ -3,6 +3,7 @@ package com.lrium.controller;
 import com.lrium.pojo.Result;
 import com.lrium.pojo.User;
 import com.lrium.service.UserService;
+import com.lrium.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -31,5 +32,22 @@ public class UserController {
                 //占用
                 return Result.error("用户名已被占用");
             }
+    }
+
+    @PostMapping("/login")
+    public Result<String> login(@Pattern(regexp = "^\\S{5,16}$") String username,@Pattern(regexp = "^\\S{5,16}$")  String password){
+        //根据用户名查询用户
+        User loginUser = userService.findByUserName(username);
+        //判断该用户是否存在
+        if (loginUser == null){
+            return Result.error("用户名错误");
+        }
+        //判断密码是否正确
+        if (Md5Util.getMD5String(password).equals(loginUser.getPassword())){
+            //登录成功
+            return Result.success("jwt token令牌..");
+        }
+
+        return Result.error("密码错误");
     }
 }
