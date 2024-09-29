182b09f2f97d239d93a6648ac93d6d2ca55e30b2
dosukoi <project.lrium@gmail.com>
2024-09-29 11:14:23 +0800

    文章分类增（）查改

--- src/main/java/com/lrium/service/CategoryService.java
+++ src/main/java/com/lrium/service/CategoryService.java
@@ -2,8 +2,19 @@ package com.lrium.service;
 
 import com.lrium.pojo.Category;
 
+import java.util.List;
+
 public interface CategoryService {
 
     //新增分类
     void add(Category category);
+
+    //列表查询
+    List<Category> list();
+
+    //根据ID查询分类信息
+    Category findById(Integer id);
+
+    //更新分类
+    void update(Category category);
 }
--- src/main/java/com/lrium/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/lrium/service/impl/CategoryServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -26,5 +27,24 @@ public class CategoryServiceImpl implements CategoryService {
         Integer userId = (Integer) map.get("id");
         category.setCreateUser(userId);
         categoryMapper.add(category);
+    }
+
+    @Override
+    public List<Category> list() {
+        Map<String ,Object> map = ThreadLocalUtil.get();
+        Integer userId = (Integer) map.get("id");
+        return categoryMapper.list(userId);
+    }
+
+    @Override
+    public Category findById(Integer id) {
+        Category c = categoryMapper.findById(id);
+        return c;
+    }
+
+    @Override
+    public void update(Category category) {
+        category.setUpdateTime(LocalDateTime.now());
+        categoryMapper.update(category);
     }
 }
